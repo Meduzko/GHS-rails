87f748d1f6875edf6defaf790033fbf511fd7fbf
root <vl.beshta@gmail.com>
2018-11-05 15:17:33 +0200

    add schema, resolve conflict

--- db/schema.rb
+++ db/schema.rb
@@ -111,12 +111,6 @@ ActiveRecord::Schema.define(version: 2018_10_31_113832) do
     t.string "category"
   end
 
-  create_table "specials", force: :cascade do |t|
-    t.string "doctype"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
